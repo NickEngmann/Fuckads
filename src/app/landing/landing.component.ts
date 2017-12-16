f15fa72f0430934fa52484215de4aecc005d91b4
Nick Engmann <NickEngmann@users.noreply.github.com>
2017-12-15 23:22:51 -0800

    added landing page component

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent {
+  title = '"Why Targeted Ads?"';
+}
