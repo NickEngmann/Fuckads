24ebebf0b753a5f4a5f0d88f9901958e15213fae
Marisol <marisol@lotus-tree>
2026-02-23 05:45:14 +0000

    Improve: package.json, app.component.ts, app.component.spec.ts (+1 more)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = '"Why Targeted Ads?"';
+  title = 'app';
   newSession = false;
   initialLanding = true;
   constructor() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { ExplainationComponent } from './explaination/explaination.component';
-import { FormComponent } from './form/form.component';
 import { LandingComponent } from './landing/landing.component';
-import {CarouselService} from './carousel/carousel.service';
+import { CarouselService } from './carousel.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent,
-    ExplainationComponent,
-    FormComponent,
-    LandingComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    [BrowserAnimationsModule],
+    BrowserAnimationsModule,
     MatButtonModule
   ],
   providers: [CarouselService],
