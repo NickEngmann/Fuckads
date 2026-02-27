962ce308e7f86beb6a19dececb9d354e0a763d2a
Marisol <marisol@lotus-tree>
2026-02-27 08:59:27 +0000

    Improve: carousel.component.ts

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,67 +1,63 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ElementRef, Renderer2 } from '@angular/core';
 import { CarouselService } from './carousel.service';
-declare var jquery:any;
-declare var $ :any;
-declare var currdeg:any;
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-
 export class CarouselComponent implements OnInit {
   @Output() onDatePicked: EventEmitter<any> = new EventEmitter<any>();
   id = 0;
-  carouselArray = ["USA", "UK", "Australia", "Germany", "France", "Brazil"]
-  carousel = $(".carousel");
-  currdeg  = 0;
-  constructor(private carouselservice: CarouselService) {
-
-  }
+  carouselArray = ["USA", "UK", "Australia", "Germany", "France", "Brazil"];
+  currdeg = 0;
+  
+  constructor(
+    private carouselservice: CarouselService,
+    private elRef: ElementRef,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit() {
   }
+  
   pickDate(date: any): void {
     this.onDatePicked.emit(date);
   }
-  changeLayout(){
-    this.pickDate("swag")
+  
+  changeLayout(): void {
+    this.pickDate("swag");
   }
-  next(){
-    $(".next").on("click", { d: "n" }, this.rotate(1, this.currdeg));
-    if(this.id == 5){
-      this.id = 0
+  
+  next(): void {
+    if (this.id == 5) {
+      this.id = 0;
+    } else {
+      this.id++;
     }
-    else{
-      this.id++
-    }
-    this.carouselservice.update(this.id)
+    this.currdeg = this.currdeg - 60;
+    this.updateCarouselRotation();
+    this.carouselservice.update(this.id);
   }
-  prev(){
-    $(".prev").on("click", { d: "p" }, this.rotate(2, this.currdeg));
-    if(this.id == 0){
+  
+  prev(): void {
+    if (this.id == 0) {
       this.id = 5;
-    }
-    else{
+    } else {
       this.id--;
     }
-    this.carouselservice.update(this.id)
+    this.currdeg = this.currdeg + 60;
+    this.updateCarouselRotation();
+    this.carouselservice.update(this.id);
   }
-  rotate(num, currdeg){
-    this.carousel = $(".carousel");
-    if(num == 1){
-      this.currdeg = currdeg - 60;
+  
+  private updateCarouselRotation(): void {
+    const carousel = this.elRef.nativeElement.querySelector('.carousel');
+    if (carousel) {
+      this.renderer.setStyle(carousel, 'transform', `rotateY(${this.currdeg}deg)`);
+      this.renderer.setStyle(carousel, '-webkit-transform', `rotateY(${this.currdeg}deg)`);
+      this.renderer.setStyle(carousel, '-moz-transform', `rotateY(${this.currdeg}deg)`);
+      this.renderer.setStyle(carousel, '-o-transform', `rotateY(${this.currdeg}deg)`);
     }
-    if(num == 2){
-      this.currdeg = currdeg + 60;
-    }
-    this.carousel.css({
-      "-webkit-transform": "rotateY("+this.currdeg+"deg)",
-      "-moz-transform": "rotateY("+this.currdeg+"deg)",
-      "-o-transform": "rotateY("+this.currdeg+"deg)",
-      "transform": "rotateY("+this.currdeg+"deg)"
-    });
   }
-
 }
